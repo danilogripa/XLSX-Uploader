54cc680cd7eb058059c0a9db655d97f2074014f2
Danilo Gripa <danilogripa@gmail.com>
2022-07-13 20:58:17 -0300

    connecting excel with request

--- excel.py
+++ excel.py
@@ -1,10 +1,24 @@
 # Import pandas
 import pandas as pd
+import request
 
-# Load the xlsx file
-excel_data = pd.read_excel("static/files/Template.xlsx")
-# Read the values of the file in the dataframe
-data = pd.DataFrame(excel_data, columns=['os', 'ag', 'type', 'key', 'tg', 'af'])
-# Print the content
+cols = ['os', 'ag', 'type', 'key', 'tg', 'af']
 
-print("The content of the file is:\n", data['os'][0])
+
+def read_excel(path: str = "static/files/Template.xlsx") -> object:
+    excel_data = pd.read_excel(path)
+    data = pd.DataFrame(excel_data, columns=cols)
+    return data
+
+
+def write_excel(data2: object, path: str = "static/files/Template2.xlsx"):
+    df = pd.DataFrame(data2)
+    writer = pd.ExcelWriter(path, engine='xlsxwriter')
+    df.to_excel(writer, sheet_name='Sheet1', index=False)
+    writer.save()
+
+
+data = read_excel()
+data["Link"] = request.send(data['os'][0], data['ag'][0], data['type'][0], data['key'][0], data['tg'][0], data['af'][0])
+write_excel(data, "static/files/Template2.xlsx")
+print(data)
