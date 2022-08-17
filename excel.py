0999537edf86d90f36ed29ec7678b4351c49f29e
Danilo Gripa <danilogripa@gmail.com>
2022-08-17 08:50:49 -0300

    adding redirect parameter

--- excel.py
+++ excel.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import request
 
-cols = ['os', 'ag', 'type', 'key', 'tg', 'af', 'Link']
+cols = ['red', 'os', 'ag', 'type', 'key', 'tg', 'af', 'Link']
 
 
 def read_excel(path: str = "static/files/Template.xlsx") -> object:
@@ -20,8 +20,8 @@ def write_excel(data: object, path: str = "static/files/Template2.xlsx"):
 def create(path: str = "static/files/Template.xlsx"):
     data: object = read_excel(path)
     for ind in data.index:
-        data["Link"][ind] = request.send(data['os'][ind], data['ag'][ind], data['type'][ind], data['key'][ind],
-                                         data['tg'][ind], data['af'][ind])
+        data["Link"][ind] = request.send(data['red'][ind], data['os'][ind], data['ag'][ind], data['type'][ind],
+                                         data['key'][ind], data['tg'][ind], data['af'][ind])
 
     path_end = path.replace(".xlsx", "") + "_final" + ".xlsx"
     write_excel(data, path_end)
--- request.py
+++ request.py
@@ -1,8 +1,8 @@
 import requests
 
 
-def send(os, ag, tp, key, tg, af):
+def send(red, os, ag, tp, key, tg, af):
     url = 'http://127.0.0.1:5000/convert'
-    myobj = {"os": os, "ag": ag, "type": tp, "key": key, "tg": tg, "af": af}
+    myobj = {"red": red, "os": os, "ag": ag, "type": tp, "key": key, "tg": tg, "af": af}
     x = requests.post(url, json=myobj)
     return x.text
